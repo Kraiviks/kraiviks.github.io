9606c22d64420fc15cfef78fc09b947a14d81dc9
Your NameKraiviks <kraiviks@gmail.com>
2020-07-05 06:34:17 +0300

    update

--- My project/src/js/script.js
+++ My project/src/js/script.js
@@ -11,4 +11,36 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
 
     openLeftNav();
+
+    class MenuCard {
+        constructor(tittle, parentSelector, ...classes) {
+            this.tittle = tittle;
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
+        }
+
+        render() {
+            const element = document.createElement('section');
+
+            if (this.classes.length === 0) {
+                this.classes = ['close'];
+            }
+
+            this.classes.forEach(className => element.classList.add(className));
+
+            element.innerHTML = `${this.tittle}`;
+            this.parent.append(element);
+        }
+    }
+    
+    new MenuCard(
+        'About me',
+        'article',
+        'hide'
+    ).render();
+    new MenuCard(
+        'My scills',
+        'article',
+        'hide'
+    ).render();
 });
